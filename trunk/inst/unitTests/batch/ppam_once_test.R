22675c0733e3564f634a9b93048c3233fa2e3380
eilidh (Ms ) <e.troup@epcc.ed.ac.uk>
2013-04-02 10:38:37 +0000

    Added ppam tests in a loop.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sprint@101 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: c28997d53f502e3b745e7e10ec6b9a474894423a

--- trunk/inst/unitTests/batch/ppam_once_test.R
+++ trunk/inst/unitTests/batch/ppam_once_test.R
@@ -27,40 +27,38 @@ data <- exprs(Golub_Merge)
 #library(ff)
 
 # test pcor and ppam together
-test.pcor_and_ppam <- function(){
+test.ppam_once_test <- function(){
+	sink(file = "/Users/egrant1/Documents/SPRINT/workspace/sprint/trunk/test_suite/ppam_once_results", append = TRUE, type = c("output", "message"),
+		 split = FALSE)
 	
 stime <- proc.time()["elapsed"]
 # Calculate distance matrix using correlation function. The parallel version writes
 # its output to a file that is loaded as an ff object in R and behaves (almost) 
 # as if data was stored in RAM
 tdata <- t(data)
-distance_matrix <- cor(tdata)
-pdistance_matrix <- pcor(tdata)
-
-class(distance_matrix)
-class(pdistance_matrix)
 	
-	invisible(checkEqualsNumeric(distance_matrix, pdistance_matrix[,]))
+distance_matrix <- cor(tdata)
+pam_result <- pam(distance_matrix, k=6, diss=TRUE)
+	
+pdistance_matrix <- pcor(tdata)
+	
+invisible(checkEqualsNumeric(distance_matrix, pdistance_matrix[,]))
 
 
 # Force memory clean-up
-gc(reset=TRUE, verbose=FALSE)
+	gc(reset=TRUE, verbose=FALSE)
 
 
 # Find 6 medoids using the PAM algortithm
 # You are passing a distance matrix on input, so set the diss parameter to TRUE
 # The parallel version of the algorithm will automatically detect the format of
 # the input data so no additional parameter is required
-pam_result <- pam(distance_matrix, k=6, diss=TRUE)
 ppam_result <- ppam(pdistance_matrix, k=6)
 
-	print(paste(pam_result$medoids))
-	print(paste(ppam_result$medoids))
-	
 checkEquals(pam_result$medoids,
-ppam_result$medoids, " Medoids should have same names.")
+			ppam_result$medoids, " Medoids should have same names.")
 
-}
+	}
 # do further analysis of data then exit MPI
 # ...
 #
