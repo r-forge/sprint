546715e9f5a6caa2d06359dc56e4ec73a9b1f89d
eilidh (Ms ) <e.troup@epcc.ed.ac.uk>
2013-06-26 12:50:45 +0000

    added system tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sprint@120 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 9a9bae5abb44227d1b763abb834feeb57eb3b212

--- branches/pStringDist/inst/systemTest/pcor/pcor_sys_test.R
+++ branches/pStringDist/inst/systemTest/pcor/pcor_sys_test.R
@@ -1,4 +1,5 @@
 library("sprint")
+library("ff")
 args <- commandArgs(trailingOnly = TRUE)
 rows<- as.numeric(args[1])
 print("number of rows")
@@ -6,11 +7,13 @@ print(rows)
 cols<- as.numeric(args[2])
 print("number of columns")
 print(cols)
+filename<- args[3]
+print("filename")
+print(filename)
 stime <- proc.time()["elapsed"]
-my.matrix <- matrix(rnorm(10000,9,1.7), nrow=rows, ncol=cols)
+my.matrix <- matrix(rnorm(500000,9,1.7), nrow=rows, ncol=cols)
 etime <- proc.time()["elapsed"]
 print(paste("Correlation time: ")); print(paste(etime-stime))
-genecor <- pcor( t(my.matrix), filename_  ="pcor.out" )
-print(system("ls -lh|grep pcor.out"))
-#print(genecor)
-
+genecor <- pcor( t(my.matrix), filename_  =filename )
+print(system(paste("ls -lh|grep ",filename)))
+system(paste("rm ",filename))
