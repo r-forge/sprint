2725f0a95b33bd9df0fd11026a921092b9eed7e4
eilidh (Ms ) <e.troup@epcc.ed.ac.uk>
2014-02-12 15:12:40 +0000

    Moved test_suite to inst/test_suite to pass R CMD check.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sprint@148 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 64ea1ec158afa6e9a5fc5951aaa2ec3373899464

--- trunk/test_suite/batchRunTests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for (( c=1; c<=10; c++ ))
-do
-  /usr/local/bin/mpiexec -n 4 /usr/local/bin/R -f /Users/egrant1/Documents/SPRINT/workspace/sprint/trunk/test_suite/batch_ppam_once_test_suite.R  
-done
